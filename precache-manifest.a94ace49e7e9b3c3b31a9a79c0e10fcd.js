4a2776dd1a22d7e9b0e32eb46aa3dabc730218fa
Eugene Leong <honghao@hotmail.sg>
2020-05-10 23:49:37 +1000

    Updates

---
+++ precache-manifest.a94ace49e7e9b3c3b31a9a79c0e10fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3327c3628fbeacd6cd8ee65f274841a",
+    "url": "/Week-19-HW/index.html"
+  },
+  {
+    "revision": "34faa8b1a78d1bf1ad88",
+    "url": "/Week-19-HW/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53e705595fb405f0fc73",
+    "url": "/Week-19-HW/static/js/2.f991075f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Week-19-HW/static/js/2.f991075f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34faa8b1a78d1bf1ad88",
+    "url": "/Week-19-HW/static/js/main.06dfd82c.chunk.js"
+  },
+  {
+    "revision": "27d0ee0e2518d97a11fc",
+    "url": "/Week-19-HW/static/js/runtime-main.1dc272e0.js"
+  }
+]);
